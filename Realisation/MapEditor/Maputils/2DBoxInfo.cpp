836de0b12da7a813ae0803ec1f4b9b23764ee37b
Alexander Freeman <Rubberduck313@gmail.com>
2016-03-30 16:18:03 +0200

    Fixed wrong enum type

--- Realisation/MapEditor/Maputils/2DBoxInfo.cpp
+++ Realisation/MapEditor/Maputils/2DBoxInfo.cpp
@@ -5,9 +5,10 @@
     right_down = down;
     state = new_state;
 }
-2DBoxInfo::2DBoxInfo(Box box, State state) {
+2DBoxInfo::2DBoxInfo(Box box, TileType new_state) {
     int deltax = box.getTopRght().get_x() - box.getBottomLeft().get_x();
     int deltay = box.getTopRght().get_y() - box.getBottomLeft().get_y();
     left_up = Coordinate(box.getBottomLeft() + deltax, box.getBottomLeft().getY(), 0);
     right_down = Coordinate(box.getTopRight().get_x(), box.getTopRight().get_y() - deltay, 0);
+    state = new_state;
 }
