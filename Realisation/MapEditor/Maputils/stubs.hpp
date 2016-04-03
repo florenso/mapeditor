fd31029f0999f5794a08135b07cbb682141349da
Alexander Freeman <Rubberduck313@gmail.com>
2016-04-03 09:56:25 +0200

    Fixed syntax errors in stubs.hpp

--- Realisation/MapEditor/Maputils/stubs.hpp
+++ Realisation/MapEditor/Maputils/stubs.hpp
@@ -1,7 +1,27 @@
 #pragma once
 
-#include <rand>
-#include <time>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+class BoxInfo {
+public:
+    BoxInfo(bool has_unknown, bool has_obstacles, bool has_navigatable) : has_unknown(has_unknown), has_obstacles(has_obstacles), has_navigatable(has_navigatable) {}
+    const bool has_unknown;
+    const bool has_obstacles;
+    const bool has_navigatable;
+};
+
+
+class Coordinate {
+public:
+    Coordinate(double x, double y, double z) : x(x), y(y), z(z) {};
+    double get_x() {return x;}
+    double get_y() {return y;}
+    double get_z() {return z;}
+private:
+    double x, y, z;
+};
 
 class Box {
 public:
@@ -15,42 +35,24 @@ public:
 private:
     Coordinate bottomLeft;
     Coordinate topRight;
-}
-
-class BoxInfo {
-public:
-    BoxInfo(bool has_unknown, bool, has_obstacles, bool has_navigatable) : has_unknown(has_unknown), has_obstacles(has_obstacles), has_navigatable(has_navigatable) {}
-    const bool has_unknown;
-    const bool has_obstacles;
-    const bool has_navigatable;
-}
-
-class Coordinate {
-public:
-    Box(double x, double y, double z) : x(x), y(y), z(z) {}
-    double get_x() {return x;}
-    double get_y() {return y;}
-    double get_z() {return z;}
-private:
-    double x, y, z;
-}
+};
 
 class Map {
 public:
     Map();
     BoxInfo get_box_info(Box box) {
-        std::srand(std::time(0))
+        std::srand(std::time(0));
         int getal = std::rand() % 3;
         switch(getal) {
             case 0: {
-                return BoxInfo(true, false false)
-            },
+                return BoxInfo(true, false, false);
+            }
             case 1: {
-                return BoxInfo(false, true false)
-            },
+                return BoxInfo(false, true, false);
+            }
             case 2: {
-                return BoxInfo(true, false true)
+                return BoxInfo(true, false, true);
             }
         }
     }
-}
+};
