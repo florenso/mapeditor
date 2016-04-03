ea6d4747d9de5614656df8d30609667845032c9a
Alexander Freeman <Rubberduck313@gmail.com>
2016-04-03 09:43:06 +0200

    Added grid-from-map function

--- Realisation/MapEditor/Maputils/map_utils.cpp
+++ Realisation/MapEditor/Maputils/map_utils.cpp
@@ -1,47 +1,76 @@
 #include "map_utils.hpp"
+#include "stubs.hpp"
 
-std::vector<RectInfo> RectInfo_from_map_using_tiles(Map map, Box size) {
-    std::vector<RectInfo> boxes;
-    sizex = size.getTopRight().get_x() - size.getBottomLeft.get_x();
-    sizey = size.getTopRight().get_y() - size.getBottomLeft.get_y();
-    sizez = size.getTopRight().get_z() - size.getBottomLeft.get_z();
-
-    Box origin_box(Coordinate(0,0,0), Coordinate(sizex, sizey, sizez))
-
+RectInfo[][] RectInfo_from_map_using_tiles(Map map, int amountx, int amounty) {
+    RectInfo[amounty][amountx] result;
     Box aabb = map.get_bounding_box(); //Axis Aligned Bounding Box
-    totalx = aabb.getTopRight().get_x() - aabb.getBottomLeft.get_x();
-    totaly = aabb.getTopRight().get_y() - aabb.getBottomLeft.get_y();
-    totalz = aabb.getTopRight().get_z() - aabb.getBottomLeft.get_z();
+    int deltax = aabb.getTopRight().get_x() - aabb.getBottomLeft().get_x();
+    int deltay = aabb.getBottomLeft().get_y() - aabb.getTopRight().get_y();
 
-    for (y = 0; y < (totaly / sizey); y++) {
-            for (x = 0; y < (totalx / sizex); x++) {
-                BoxInfo info = map.get_box_info();
-                int i = 0;
-                std::map<std::string, std::string> animals;
-                if (info.has_navigatable) i++;
-                if (info.has_unknown) i++;
-                if (info.has_obstacles) i++;
+    int sizex = deltax / amountx;
+    int sizey = deltay / amounty;
 
-                if (i > 1) {
-                    //Smaller resolution
-                }
-                else {
-                    int x_l = sizex * x;
-                    int y_l = sizey * y;
-                    int x_r = (x + 1) * sizex;
-                    int y_r = (y + 1) * sizey
-                    int z_r = sizez;
+    for (int y = 0; y < amounty; y++) {
+        for (int x = 0; x < amountx; x++) {
+            Box current_box = Box(Coordinate(sizex * x, sizey * y, 0), Coordinate(sizex * (x + 1), sizey * (y + 1), 1))
+            BoxInfo info = map.get_box_info(current_box);
+            TileType new_type = TileType::UNKNOWN;
+            if (info.has_navigatable)   new_type = TileType::EMPTY;
+            if (info.has_obstacles)     new_type = TileType::BLOCKED;
+            result[y][x] = RectInfo(current_box, new_type);
+        }
+    }
+}
 
-                    TileType new_type = TileType::None;
-                    if (info.has_navigatable) new_type = TileType::EMPTY;
-                    if (info.has_unknown) new_type = TileType::UNKNOWN;
-                    if (info.has_obstacles) new_type = TileType::BLOCKED;
+RectInfo[][] RectInfo_from_map_using_quadtree(Map map, Box size) {
+    std::vector<RectInfo> boxes;
+    BoxInfo info = map.get_box_info(current_box);
+    RectInfo rectinfo = RectInfo(aabb, TileType::UNKNOWN);
 
-                    Box new_box = Box(Coordinate(x_l, y_l, 0),
-                                    Coordinate(x_r, y_r, z_r))
-                    boxes.push_back(RectInfo(new_box, new_type);
-                }
-            }
+    return get_rectinfo_recursive(map, aabb, size, rectinfo);
+}
+
+//NOT FINISHED
+RectInfo get_rectinfo_recursive(Map map, Box total, RectInfo& parent) {
+
+    sizex = total.getTopRight().get_x() - total.getBottomLeft().get_x();
+    sizey = total.getTopRight().get_y() - total.getBottomLeft().get_y();
+
+    // Box origin_box(Coordinate(0,0,0), Coordinate(sizex, sizey, 1))
+
+    int i = 0;
+
+    TileType new_type = TileType::UNKNOWN;
+    if (info.has_navigatable)   new_type = TileType::EMPTY;
+    if (info.has_unknown)       new_type = TileType::UNKNOWN;
+    if (info.has_obstacles)     new_type = TileType::BLOCKED;
+
+    if (info.has_navigatable)   i++;
+    if (info.has_unknown)       i++;
+    if (info.has_obstacles)     i++;
+
+    if (i > 1) {
+        new_type = TileType::MIXED;
     }
 
+    RectInfo current_info = RectInfo(new_box, new_type);
+
+    if (i > 1) {
+        sizex /= 2;
+        sizey /= 2;
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                //Omzetten naar de goede hoeken :/
+                current_box = Box(Coordinate(total.getBottomLeft().get_x() + , total.getTopRight().get_y(), 0),
+                                  Coordinate(total.getTopRight().get_x(), total.getBottomLeft().get_y(), 1))
+            }
+        }
+        current_box = Box(Coordinate(x_l, y_l, 0),
+                        Coordinate(x_r, y_r, 1))
+        RectInfo new_info = get_rectinfo_recursive(map, current_box, info);
+        boxes.push_back(info);
+    }
+    else {
+        parent.add_rekt(current_info);
+    }
 }
--- Realisation/MapEditor/Maputils/map_utils.hpp
+++ Realisation/MapEditor/Maputils/map_utils.hpp
@@ -3,6 +3,8 @@
 #ifndef MAPUTILS
 #define MAPUTILS
 
+RectInfo[][] RectInfo_grid_from_map(Map map, int amountx, int amounty);
 std::vector<RectInfo> RectInfo_from_map_using_tiles(Map map);
+RectInfo get_rectinfo_recursive(Map map, Box total, RectInfo& parent)
 
 #endif /* end of include guard:  */
