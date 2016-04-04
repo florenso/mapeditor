4c99bc45f26692b89cc3dec217426b0b642bfde6
Alexander Freeman <Rubberduck313@gmail.com>
2016-04-04 13:30:34 +0200

    Fixed the final syntax error

--- Realisation/MapEditor/Maputils/map_utils.hpp
+++ Realisation/MapEditor/Maputils/map_utils.hpp
@@ -3,8 +3,8 @@
 #ifndef MAPUTILS
 #define MAPUTILS
 
-std::vector<std::vector<RectInfo>> RectInfo_grid_from_map(MapTypes::TileType map, int amountx, int amounty);
-std::vector<std::vector<RectInfo>> RectInfo_from_map_using_tiles(Map map);
+std::vector<std::vector<RectInfo> > RectInfo_grid_from_map(MapTypes::TileType map, int amountx, int amounty);
+std::vector<std::vector<RectInfo> > RectInfo_from_map_using_tiles(Map map);
 RectInfo get_rectinfo_recursive(Map map, Box total, RectInfo& parent);
 
 #endif /* end of include guard:  */
