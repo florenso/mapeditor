17400561c813618d137e51aa9a40d047a10c71cc
Alexander Freeman <Rubberduck313@gmail.com>
2016-04-23 16:01:36 +0200

    Added class doxygen to mapeditor.hpp

--- source/include/mapEditor.hpp
+++ source/include/mapEditor.hpp
@@ -9,10 +9,19 @@
 #include "mapView.hpp"
 //#include "stubs.hpp"
 
+//! The class that represents a map editor for the r2d2 map.
+/*!
+ * The mapEditor class represents a mapeditor for the r2d2 map.
+ * It extends the mapView class and adds functionality that allows the user
+ * to edit the maps as well. It lets the user change tiles to various types,
+ * like BLOCKED, NAVIGATABLE or MIXED.
+ */
 class mapEditor : public mapView
     {
     //Q_OBJECT
 public:
+    //! Constructor of the mapeditor object
+    //! @param parent A pointer to the parent QWidget
     explicit mapEditor(QWidget *parent = 0);
     void createTile();
     void removeTile();
