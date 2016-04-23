7a71ccbe436b4ddb60242c1416bbc163426df3fe
Alexander Freeman <Rubberduck313@gmail.com>
2016-04-23 14:47:05 +0200

    Added doxygen for MapView.hpp

--- source/include/mapView.hpp
+++ source/include/mapView.hpp
@@ -44,43 +44,91 @@ class mapView: public QGraphicsView
 {
 public:
     //! Constructor of the mapView.
+    //! @param parent A pointer to the parent QWidget
     explicit mapView(QWidget *parent);
 
-    //! Deconstroctor of the mapView.
-    ~mapView();   
+    //! Destructor of the mapView.
+    ~mapView();
 
+    //! Increases the scale of the mapviewer by zooming out.
+    //! It will zoom out with the current zoomspeed.
     void increaseScale();
+
+    //! Increases the scale of the mapviewer by zooming in.
+    //! It will zoom in with the current zoomspeed.
     void decreaseScale();
+
+    //! Adds an angle to the current rotation of the mapviewer.
+    //! Also updates the current view.
+    //! @param inc The rotation to add.
     void increaseRotation(int inc = 90);
+
+    //! Subtracts an angle to the current rotation of the mapviewer.
+    //! Also updates the current view.
+    //! @param inc The rotation to subtract.
     void decreaseRotation(int dec = 90);
+
+    //! Resets the rotation to its original value.
+    //! Also updates the view.
     void resetRotation();
+
+    //! Resets the scale to its origin value.
+    //! Also updates the view.
     void resetScale();
+
+    //! Sets the zoomspeed to a given value.
+    //! @param speed The zoomspeed as a Qreal
     void setZoomSpeed(qreal speed);
+
+    //! Returns the current scale as an int
     int getScale();
+
+    //! Returns the current rotation as an int
     int getRotation();
+
+    //! Checks if the current view is near the borders of the loaded map.
+    //! If that is the case, it loads an extra piece of the map and
+    //! draws the map and axes correctly.
     void checkSceneBorder();
+
+    //! Sets the lowest z coordinate of the area that is shown on the map.
+    //! @param value The z coordinate that is to be set
     void set_z_bottom(float value);
+
+    //! Sets the highest z coordinate of the area that is shown on the map.
+    //! @param value The z coordinate that is to be set
     void set_z_top(float value);
 
+    //! The current Scene the map is drawn upon.
     viewScene * scene;
 
+    //! A std::map containing the coloring of the possible tiles in the map.
     std::map<MapTypes::TileType, QColor> tileColors;
 
-    //this has a temp function, it generates a map for testing
+    //! Loads the map into the viewer using file i/o.
+    //! @param file The file to load the map from.
     void loadMapFile(string file);
 
-    //focuses viewport on origin of map and (re)draws the map
+    //! Draws the current map on the viewer.
+    //! Resets scale and rotation and focuses on the center of the map.
     void drawMap();
 
+    //! A helper method that obtains the tiletype from information about
+    //! a piece of the map.
+    //! @param tileInfo The BoxInfo from which to get the tiletype.
     MapTypes::TileType getTileType(r2d2::BoxInfo & tileInfo);
 
+    //! Draws an area of the map by cutting it in pieces and obtaining their
+    //! tiletypes.
+    //! @param box The area to load and draw.
+    //! @param tileSize The amount of pieces to cut the area into.
     void drawBox(r2d2::Box box, int tileSize = 10);
 private:
 
 
     void updateTransform();
     int windowWidth;
-    int windowHeight;  
+    int windowHeight;
     int rotation  = 0;
     qreal zoomSpeed = 0.05f;
     qreal maxScale  = 5.0f;
@@ -91,8 +139,14 @@ private:
 protected:
     //! Catches all events and returns true when a event is caught
     bool event(QEvent *event);
+
+    //! Returns if you want to stop the event from going to other objects.
     bool eventFilter(QObject *object, QEvent *event);
+
+    //! The bottom z coordinate of the loaded map
     float z_bottom = 0;
+
+    //! The top z coordinate of the loaded map
     float z_top = 1;
     };
 
