0f251c1b24475a9a9653e396e8277e1b198af9e9
koen de gruijter <koen.degruijter@student.hu.nl>
2016-05-16 23:27:10 +0200

    added licence

--- source/include/mainwindow.hpp
+++ source/include/mainwindow.hpp
@@ -1,20 +1,56 @@
-//**************************************
-//! Class responsible for dispaying all the UI elements
-//! \authors
-//! 	- Daniel Klomp
-//!		- Jop van Buuren
-//!     - Koen de Guijter
-//!     - jessy Visch
+//! \addtogroup 0008 Mapviewer/-editor
+//! \brief main gui class
 //!
-//! \context
-//!		- part of TH7-8 assignment 2016 / Project: MapEditer
-//**************************************
+//! this class extends QMainWindow.
+//! in this class are all the buttons, labels and other gui items declared that are in the gui.
+//!
+//! \file   mainwindow.hpp
+//! \author jessy Visch,
+//!         Jop van Buuren, 1658718
+//!         Daniel Klomp, 1661521
+//!         Koen de Guijter, 1671103
+//! \date   Created: 30-03-2016
+//! \date   Last Modified: 12-05-2016
+//! \brief  Header for mainwindow
+//!
+//! This is the header file for mainwindow
+//!
+//! \copyright Copyright © 2016, HU University of Applied Sciences Utrecht.
+//! All rights reserved.
+//!
+//! License: newBSD
+//!
+//! Redistribution and use in source and binary forms,
+//! with or without modification, are permitted provided that
+//! the following conditions are met:
+//! - Redistributions of source code must retain the above copyright notice,
+//!   this list of conditions and the following disclaimer.
+//! - Redistributions in binary form must reproduce the above copyright notice,
+//!   this list of conditions and the following disclaimer in the documentation
+//!   and/or other materials provided with the distribution.
+//! - Neither the name of the HU University of Applied Sciences Utrecht
+//!   nor the names of its contributors may be used to endorse or promote
+//!   products derived from this software without specific prior written
+//!   permission.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+//! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+//! AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//! IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
+//! BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+//! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//! EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ~< HEADER_VERSION 2016 04 12 >~
 
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
-//#include "mapView.hpp"
 #include "mapEditor.hpp"
 #include <QDesktopServices>
 #include <QFileDialog>
@@ -56,15 +92,9 @@ private slots:
 
     void on_actionLoad_triggered();
 
-   // void on_graphicsView_rubberBandChanged(const QRect &viewportRect, const QPointF &fromScenePoint, const QPointF &toScenePoint);
-
     void on_zoomInButton_clicked();
 
     void on_zoomOutButtom_clicked();
-
-  //  void on_inputX_cursorPositionChanged(int arg1, int arg2);
-  //  void on_inputY_cursorPositionChanged(int arg1, int arg2);
-
 
     //! Enables the drag mode for the map viewer
     void on_actionPan_toggled(bool);
@@ -98,7 +128,9 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+
     r2d2::SaveLoadMap * map;
+
     std::string fileName_std = "";
 };
 
